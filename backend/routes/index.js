5f0fb1f54b2cbb1a0227026115ba5bab279009e1
Logan.Frye <Loganfrye714@gmail.com>
2021-06-16 15:50:41 -0700

    backend-boilerplate-done-part1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,7 +10,14 @@ router.get('/hello/world', function(req, res) {
 
 router.use('/api', apiRouter);
 
-
+// fetch('/api/test', {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+//   },
+//   body: JSON.stringify({ hello: 'world' })
+// }).then(res => res.json()).then(data => console.log(data));
 
 
 
