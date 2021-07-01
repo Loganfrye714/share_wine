a34f4a121bf3fbf0d228ca794b3003b63abcd21a
Logan.Frye <Loganfrye714@gmail.com>
2021-07-01 15:43:04 -0700

    navbar work

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,10 +1,14 @@
 // frontend/src/components/Navigation/index.js
 import React from "react";
+import "./Navigation.css";
+import SearchIcon from "@material-ui/icons/Search";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import LocalBarIcon from "@material-ui/icons/LocalBar";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import LoginFormModal from "../LoginFormModal";
-import "./Navigation.css";
+import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
@@ -22,14 +26,28 @@ function Navigation({ isLoaded }) {
   }
 
   return (
-    <ul>
-      <oi>
-        <NavLink exact to="/" className="logo">
-          Share Wine
-        </NavLink>
-        {isLoaded && sessionLinks}
-      </oi>
-    </ul>
+    <div className="header">
+      <ul>
+        <oi>
+          <NavLink exact to="/" className="logo">
+            Share Wine
+          </NavLink>
+        </oi>
+      </ul>
+      <div className="header__middle">
+        <input type="text" />
+        <SearchOutlinedIcon />
+      </div>
+      <div className="header__right">
+        {/* <NavLink>Wines</NavLink> */}
+        <p>Wines</p>
+        <LocalBarIcon />
+      </div>
+      <div className="header__rightend">
+        <PersonOutlineIcon />
+      </div>
+    </div>
+    // {isLoaded && sessionLinks} -> need to be in the icon
   );
 }
 
