b793bb1f2db3f3e143d0b410107e37465eda520d
Logan.Frye <Loganfrye714@gmail.com>
2021-07-07 11:44:47 -0700

    h1/h2 and header image added

--- frontend/src/components/HomePage/CampaignPage/index.js
+++ frontend/src/components/HomePage/CampaignPage/index.js
@@ -2,10 +2,16 @@ import { NavLink } from "react-router-dom";
 import "./campaignPage.css";
 
 const CampaignPageContainer = () => {
+  const headerText = "Region of the month";
+  const line2 = "Italy";
+
   return (
     <div className="campaign__grid">
       <div className="campaign__card">
-        <div className="campaign__card-header"></div>
+        <div className="campaign__card-header">
+          <h2 className="campaign__card-header-h2">Region of the month</h2>
+          <h1 className="campaign__card-header-h1">Italy</h1>
+        </div>
         <div className="campaign__card-content"></div>
       </div>
     </div>
