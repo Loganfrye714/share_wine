63b50f3c62c4ebc3934891abb75441b26d1dced3
Logan.Frye <Loganfrye714@gmail.com>
2021-07-08 09:26:59 -0700

    Region of the month card almost completed

--- frontend/src/components/HomePage/CampaignPage/index.js
+++ frontend/src/components/HomePage/CampaignPage/index.js
@@ -2,9 +2,6 @@ import { NavLink } from "react-router-dom";
 import "./campaignPage.css";
 
 const CampaignPageContainer = () => {
-  const headerText = "Region of the month";
-  const line2 = "Italy";
-
   return (
     <div className="campaign__grid">
       <div className="campaign__card">
@@ -12,7 +9,11 @@ const CampaignPageContainer = () => {
           <h2 className="campaign__card-header-h2">Region of the month</h2>
           <h1 className="campaign__card-header-h1">Italy</h1>
         </div>
-        <div className="campaign__card-content"></div>
+        <div className="campaign__card-content">
+          <p>Discover the amazing wines</p>
+          <button>Shop Now</button>
+          {/* when you hover over shop now it should show a button */}
+        </div>
       </div>
     </div>
   );
