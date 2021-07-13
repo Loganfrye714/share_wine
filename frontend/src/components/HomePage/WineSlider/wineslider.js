d83cb63a22280104b1478aab1220bcd14862cd1f
Logan.Frye <Loganfrye714@gmail.com>
2021-07-13 13:00:11 -0700

    homepage slider

--- frontend/src/components/HomePage/WineSlider/wineslider.js
+++ frontend/src/components/HomePage/WineSlider/wineslider.js
@@ -17,15 +17,16 @@ const ImageSlider = () => {
     dispatch(getWines());
   }, [dispatch]);
 
+  // if the current number reaches the end of the array (length - 1) then set current to the beginning else current + 1
   const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
 
+// if current is === 0 (original image) set the length - 1 else subtract one
   const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
+    setCurrent(current === 0 ? length - 1 : current - 1)
+  }
 
-  console.log(current);
 
   return (
     <div className="slider">
@@ -34,6 +35,75 @@ const ImageSlider = () => {
         <>
           <div className="allwines__grid" key={index}>
             {index === current && (
+              <>
+                <div className="wine__card">
+                  <img
+                    id="allwines__image"
+                    src={wine.img_url}
+                    key={wine.id}
+                    alt="wines"
+                  />
+                  <div className="wine__cardInfo">
+                    <h2>{wine.name}</h2>
+                    <h4>{wine.location}</h4>
+                    <h4>
+                      {wine.grape} {wine.vintage}
+                    </h4>
+                    <h4>${wine.price}</h4>
+                    <button id="wine__cardButton">Reserve</button>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+          <div className="allwines__grid" key={index + 1}>
+            {index === current + 1 && (
+              <>
+                <div className="wine__card">
+                  <img
+                    id="allwines__image"
+                    src={wine.img_url}
+                    key={wine.id}
+                    alt="wines"
+                  />
+                  <div className="wine__cardInfo">
+                    <h2>{wine.name}</h2>
+                    <h4>{wine.location}</h4>
+                    <h4>
+                      {wine.grape} {wine.vintage}
+                    </h4>
+                    <h4>${wine.price}</h4>
+                    <button id="wine__cardButton">Reserve</button>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+          <div className="allwines__grid" key={index + 1}>
+            {index === current + 2 && (
+              <>
+                <div className="wine__card">
+                  <img
+                    id="allwines__image"
+                    src={wine.img_url}
+                    key={wine.id}
+                    alt="wines"
+                  />
+                  <div className="wine__cardInfo">
+                    <h2>{wine.name}</h2>
+                    <h4>{wine.location}</h4>
+                    <h4>
+                      {wine.grape} {wine.vintage}
+                    </h4>
+                    <h4>${wine.price}</h4>
+                    <button id="wine__cardButton">Reserve</button>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+                    <div className="allwines__grid" key={index + 1}>
+            {index === current + 3 && (
               <>
                 <div className="wine__card">
                   <img
