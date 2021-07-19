07fe42a8d159e813e3cddefd791becf8d2b51968
Logan.Frye <Loganfrye714@gmail.com>
2021-07-19 08:43:11 -0700

    review cards start

--- frontend/src/components/SingleWinePage/singleWineCard.js
+++ frontend/src/components/SingleWinePage/singleWineCard.js
@@ -19,6 +19,18 @@ const SingleWineCard = () => {
     <>
       <div className="singleWine__grid">
         <>
+          <div className="singleWine__bio">
+            <h4>
+              ---placeholder---- Spain is the third largest wine-producing
+              country in the world after Italy and France. That means there's a
+              lot of wonderful Spanish wine to try. But have you taken time to
+              explore the depth of Spanish wines? Whether you're newly curious,
+              or you already have a wine rack full of Rioja, our guide is a
+              great tool. It will help you understand what makes Spanish wine
+              taste the way it does, and feel confident exploring all the
+              interesting vinos de España.
+            </h4>
+          </div>
           <div className="singleWine__card">
             <img
               id="singleWine__image"
@@ -33,7 +45,7 @@ const SingleWineCard = () => {
                 {wines[id].grape} {wines[id].vintage}
               </h4>
               <h4>${wines[id].price}</h4>
-              <button id="singleWine__cardButton">Reserve</button>
+              <button id="singleWine__cardButton">Wishlist</button>
             </div>
           </div>
           <div className="singleWine__map">
