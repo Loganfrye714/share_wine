9ae23f4fbb9b63087e98065ce2036e51976fbd60
Logan.Frye <Loganfrye714@gmail.com>
2021-08-02 14:00:07 -0700

    css work and other quick edits

--- frontend/src/components/Review/comments.js
+++ frontend/src/components/Review/comments.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
-import { findReviewsOneWine } from "../../store/review";
+import { findReviewsOneWine, addReview } from "../../store/review";
+import "./comments.css";
 
 const CommentsCard = () => {
   const history = useHistory();
@@ -14,12 +15,56 @@ const CommentsCard = () => {
 
   // Variables for indvidiual wine
   const reviewArray = useSelector((state) => Object.values(state.review));
+  const session = useSelector((state) => state.session);
+
+  // state for reviews
+  const [rating, setRating] = useState("");
+  const [comment, setComment] = useState("");
+  const [wineId, setWineId] = useState(id);
+  const [userId, setUserId] = useState(session.user.id);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const review = {
+      rating,
+      comment,
+      wineId,
+      userId,
+    };
+    dispatch(addReview(review));
+    window.location.reload();
+  };
 
   return (
-    <div>
-      {reviewArray.map((review) => (
-        <h1>{review.comment}</h1>
-      ))}
+    <div className="comments_grid">
+      <h1>Community reviews</h1>
+      <form onSubmit={onSubmit}>
+        <button>Post a review</button>
+        <div>
+          Comment Here:
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={(e) => setComment(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          Your Rating:
+          <input
+            type="number"
+            id="rating"
+            name="rating"
+            max="5"
+            onChange={(e) => setRating(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          {reviewArray.map((review) => (
+            <h4>{review.comment}</h4>
+          ))}
+        </div>
+      </form>
     </div>
   );
 };
