0e58c11596e87e3640281fe61b606acc7141bde7
Logan.Frye <Loganfrye714@gmail.com>
2021-08-07 13:22:57 -0700

    css work for wishlist

--- frontend/src/components/Wishlist/wishlist.js
+++ frontend/src/components/Wishlist/wishlist.js
@@ -3,11 +3,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { getOneWine } from "../../store/wines";
 import { useParams, useHistory } from "react-router-dom";
 import { findWishlist, deleteWishlist } from "../../store/wishlist";
+import "./wishlist.css";
 
 const WishlistContainer = () => {
   const dispatch = useDispatch();
   const userId = useSelector((state) => state.session.user.id);
   const wishlists = useSelector((state) => Object.values(state.wishlist));
+  const userAlias = useSelector((state) => state.session.user.username);
 
   useEffect(() => {
     dispatch(findWishlist(userId));
@@ -18,38 +20,42 @@ const WishlistContainer = () => {
   };
 
   return (
-    <div>
-      {wishlists.map((wishlist) => (
-        <div className="allwines__grid">
-          <div>
-            <button onClick={() => removeWishlist(wishlist.id)}>Remove</button>
-          </div>
-          <div className="singleWine__grid">
-            <>
-              <div className="singleWine__card">
-                <img
-                  id="singleWine__image"
-                  src={wishlist.Wine.img_url}
-                  key={wishlist.Wine.id}
-                  alt="singleWine"
-                />
-                <div className="singleWine__cardInfo">
-                  <h2>{wishlist.Wine.name}</h2>
-                  <h4>{wishlist.Wine.location}</h4>
-                  <h4>
-                    {wishlist.Wine.grape} {wishlist.Wine.vintage}
-                  </h4>
-                  <h4>${wishlist.Wine.price}</h4>
+    <>
+      <div>
+        <h1>{userAlias} wine's </h1>
+      </div>
+      <div>
+        {wishlists.map((wishlist) => (
+          <div className="wishlist__grid">
+            <div>
+              <button onClick={() => removeWishlist(wishlist.id)}>
+                Remove
+              </button>
+            </div>
+            <div>
+              <>
+                <div className="wishlist__card">
+                  <img
+                    id="wishlistWine__image"
+                    src={wishlist.Wine.img_url}
+                    key={wishlist.Wine.id}
+                    alt="singleWine"
+                  />
+                  <div className="singleWine__cardInfo">
+                    <h2>{wishlist.Wine.name}</h2>
+                    <h4>{wishlist.Wine.location}</h4>
+                    <h4>
+                      {wishlist.Wine.grape} {wishlist.Wine.vintage}
+                    </h4>
+                    <h4>${wishlist.Wine.price}</h4>
+                  </div>
                 </div>
-              </div>
-              <div className="singleWine__map">
-                <h2> Rating will go here</h2>
-              </div>
-            </>
+              </>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
 
