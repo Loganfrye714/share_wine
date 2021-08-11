70a4348e2f8429c7956de6ca955517b78d012018
Logan.Frye <Loganfrye714@gmail.com>
2021-08-11 11:27:28 -0700

    wishlist no directs to wines if no wines are there

--- frontend/src/components/Wishlist/wishlist.js
+++ frontend/src/components/Wishlist/wishlist.js
@@ -4,6 +4,7 @@ import { getOneWine } from "../../store/wines";
 import { useParams, useHistory } from "react-router-dom";
 import { findWishlist, deleteWishlist } from "../../store/wishlist";
 import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+import { NavLink } from "react-router-dom";
 
 import "./wishlist.css";
 
@@ -24,45 +25,63 @@ const WishlistContainer = () => {
     window.location.reload();
   };
 
-  return (
-    <>
-      {/* <div>
+  if (wishlists.length) {
+    return (
+      <>
+        {/* <div>
         <h1>{userAlias} wine's </h1>
       </div> */}
-      <div>
-        {wishlists.map((wishlist) => (
-          <div className="wishlist__grid">
-            <div className="wishlist__button-grid">
-              <HighlightOffIcon
-                id="wishlist__button"
-                onClick={() => removeWishlist(wishlist.id)}
-              />
-            </div>
-            <div>
-              <>
-                <div className="wishlist__card">
-                  <img
-                    id="wishlistWine__image"
-                    src={wishlist.Wine.img_url}
-                    key={wishlist.Wine.id}
-                    alt="singleWine"
-                  />
-                  <div className="singleWine__cardInfo">
-                    <h2>{wishlist.Wine.name}</h2>
-                    <h4>{wishlist.Wine.location}</h4>
-                    <h4>
-                      {wishlist.Wine.grape} {wishlist.Wine.vintage}
-                    </h4>
-                    <h4>${wishlist.Wine.price}</h4>
+        <div>
+          {wishlists.map((wishlist) => (
+            <div className="wishlist__grid">
+              <div className="wishlist__button-grid">
+                <HighlightOffIcon
+                  id="wishlist__button"
+                  onClick={() => removeWishlist(wishlist.id)}
+                />
+              </div>
+              <div>
+                <>
+                  <div className="wishlist__card">
+                    <img
+                      id="wishlistWine__image"
+                      src={wishlist.Wine.img_url}
+                      key={wishlist.Wine.id}
+                      alt="singleWine"
+                    />
+                    <div className="singleWine__cardInfo">
+                      <h2>{wishlist.Wine.name}</h2>
+                      <h4>{wishlist.Wine.location}</h4>
+                      <h4>
+                        {wishlist.Wine.grape} {wishlist.Wine.vintage}
+                      </h4>
+                      <h4>${wishlist.Wine.price}</h4>
+                    </div>
                   </div>
-                </div>
-              </>
+                </>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+          ))}
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div>
+          <h1 id="noWishlist">
+            You don't have any wines in your wishlist yet! click the below to
+            check our collection out.
+          </h1>
+        </div>
+        <div>
+          <NavLink to="/wines">
+            <button>Checkout Our Wines</button>
+          </NavLink>
+        </div>
+      </>
+    );
+  }
 };
 
 export default WishlistContainer;
